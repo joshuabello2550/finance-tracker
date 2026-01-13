30b61732eb11db2eeaf79a561fdb7d679e4fb320
Joshua Bello <joshuabello2550@gmail.com>
2026-01-13 16:49:21 -0500

    fix origins for index.py

--- api/index.py
+++ api/index.py
@@ -19,9 +19,7 @@ if os.getenv("ENV") == "development":
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
